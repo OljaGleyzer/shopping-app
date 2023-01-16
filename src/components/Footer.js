7ca71113541ff0748c080ef79f18e38b8184a31d
OljaGleyzer <o.gleyzer@gmail.com>
2023-01-16 17:32:53 +0100

    create components Navbar, Footer, Products

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,31 @@
 import React from "react";
+import {
+  MDBFooter,
+  MDBContainer,
+  MDBCol,
+  MDBRow,
+  MDBIcon,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
 function Footer() {
   return (
-    <div>
-      <p>Footer </p>
-    </div>
+    <MDBFooter
+      className="text-center text-white fixed-bottom"
+      style={{ backgroundColor: "#21081a" }}
+    >
+      <MDBContainer className="p-4"></MDBContainer>
+
+      <div
+        className="text-center p-3"
+        style={{ backgroundColor: "rgba(0, 0, 0, 0.2)" }}
+      >
+        © 2023 Copyright:
+        <a className="text-white" href="https://mdbootstrap.com/">
+          MDBootstrap.com
+        </a>
+      </div>
+    </MDBFooter>
   );
 }
-
 export default Footer;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,26 @@
 import React from "react";
+import Nav from "react-bootstrap/Nav";
 
 function Navbar() {
   return (
-    <div>
-      <h1>Shopping</h1>
-    </div>
+    <Nav
+      className="justify-content-end bg-dark"
+      variant="pills"
+      defaultActiveKey="/home"
+    >
+      <Nav.Link eventKey="disabled" disabled>
+        <p clasName="justify-content-start"> MarxitUp</p>
+      </Nav.Link>
+      <Nav.Item>
+        <Nav.Link href="/home">Login</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">Register</Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link eventKey="link-1">About</Nav.Link>
+      </Nav.Item>
+    </Nav>
   );
 }
 
