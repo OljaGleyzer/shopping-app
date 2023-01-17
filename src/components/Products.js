06bc5729e517d4039c5dee442e0deb4c35c877ec
OljaGleyzer <o.gleyzer@gmail.com>
2023-01-17 10:21:27 +0100

    fetching data

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,11 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
-function Products() {
+function ProductList() {
+  const [products, setProducts] = useState([]);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch("https://fakestoreapi.com/products");
+        const data = await response.json();
+        setProducts(data);
+      } catch (err) {
+        setError(err);
+      }
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <p>Products</p>
+      {error && <p>{error.message}</p>}
+      {products.map((product) => (
+        <div key={product.id}>
+          <img src={product.image} alt={product.title} />
+          <h2>{product.title}</h2>
+          <p>{product.price}</p>
+          <button>Show more</button>
+        </div>
+      ))}
     </div>
   );
 }
 
-export default Products;
+export default ProductList;
