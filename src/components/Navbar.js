900b229f820d509a3307966f4d8ec4994876c17b
OljaGleyzer <o.gleyzer@gmail.com>
2023-01-24 16:17:16 +0100

    Debug: stop Links in Navbar bootsstrap to refresh

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,13 +15,24 @@ function Navbar() {
         </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/login">Login</Nav.Link>
+        <Nav.Link as={Link} to="/login">
+          Login
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/register">Register</Nav.Link>
+        <Nav.Link as={Link} to="/login">
+          Logout
+        </Nav.Link>
       </Nav.Item>
       <Nav.Item>
-        <Nav.Link href="/about">About</Nav.Link>
+        <Nav.Link as={Link} to="/register">
+          Register
+        </Nav.Link>
+      </Nav.Item>
+      <Nav.Item>
+        <Nav.Link as={Link} to="/about">
+          About
+        </Nav.Link>
       </Nav.Item>
     </Nav>
   );
